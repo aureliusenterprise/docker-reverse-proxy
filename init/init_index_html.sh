e70cf1a45eebc30390336109af4f9c3ad1550251
Andreas <andreas.wombacher@aureliusenterprise.com>
2022-05-19 22:18:47 +0200

    fixed still the script

--- init/init_index_html.sh
+++ init/init_index_html.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [[ ! -v NAMESPACE ]]; then
-    mv /usr/local/apache2/htdocs/atlas/index.html /usr/local/apache2/htdocs/index_.h
-    sed "s/<base href=\"\/atlas\//<base href=\"\/$NAMESPACE\/atlas\//g" /usr/local/apache2/htdocs/index_.h > /usr/local/apache2/htdocs/index.html
-    rm -f atlas/index_.h
+if [[ -v NAMESPACE ]]; then
+    mv /usr/local/apache2/htdocs/atlas/index.html /usr/local/apache2/htdocs/atlas/index_.h
+    sed "s/<base href=\"\//<base href=\"\/$NAMESPACE\/atlas\//g" /usr/local/apache2/htdocs/atlas/index_.h > /usr/local/apache2/htdocs/atlas/index.html
+    rm -f /usr/local/apache2/htdocs/atlas/index_.h
 fi
