e16518346180d5d0efad3244023e1b44b74635ee
Andreas <andreas.wombacher@aureliusenterprise.com>
2022-05-20 22:05:09 +0200

    many changes
    - moved atlas files to a own directory
    - added atlas.conf into conf.d to get more control over the atlas application
    - extended the init script to also update the /auth to /NAMESPACE/auth in the keycloak spec of the code

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM httpd:2.4
 LABEL maintainer="andreas.wombacher@aureliusenterprise.com"
-COPY ./public-html/ /usr/local/apache2/htdocs/
+COPY ./htdocs/ /usr/local/apache2/htdocs/
+COPY ./atlas/ /usr/local/apache2/atlas/
 COPY ./init/ /usr/local/apache2/init/
 COPY ./conf/httpd.conf /usr/local/apache2/conf/httpd.conf
+COPY ./conf.d/ /usr/local/apache2/conf.d/
--- init/init_index_html.sh
+++ init/init_index_html.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 if [[ -v NAMESPACE ]]; then
-    mv /usr/local/apache2/htdocs/atlas/index.html /usr/local/apache2/htdocs/atlas/index_.h
-    sed "s/<base href=\"\//<base href=\"\/$NAMESPACE\/atlas\//g" /usr/local/apache2/htdocs/atlas/index_.h > /usr/local/apache2/htdocs/atlas/index.html
-    rm -f /usr/local/apache2/htdocs/atlas/index_.h
+    mv /usr/local/apache2/atlas/index.html /usr/local/apache2/atlas/index.orig.html
+    sed "s/<base href=\"\//<base href=\"\/$NAMESPACE\/atlas\//g" /usr/local/apache2/atlas/index.orig.html > /usr/local/apache2/atlas/index.html
+    #rm -f /usr/local/apache2/atlas/index_.h
+
+    #set -- /usr/local/apache2/atlas/main-es*.js
+    #mv "$1" /usr/local/apache2/atlas/main-es.h
+    cp /usr/local/apache2/atlas/main-es*.js /usr/local/apache2/atlas/bak/
+    sed -i "s/url:\"\/auth\"/url:\"\/$NAMESPACE\/atlas\"/g" /usr/local/apache2/atlas/main-es*.js
 fi
